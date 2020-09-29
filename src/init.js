6a27e460ce09a77f3f63f4657d65208df5741f3c
Sandra <sandramoroz@yandex.ru>
2020-09-29 18:27:20 +0300

    add rss parser

--- src/init.js
+++ src/init.js
@@ -3,37 +3,100 @@ import './scss/app.scss';
 
 import * as yup from 'yup';
 import axios from 'axios';
+import i18next from 'i18next';
+import _ from 'lodash';
 
 import watch from './watchers.js';
 
+const parse = (data) => {
+  const parser = new DOMParser();
+  const dataParsed = parser.parseFromString(data, 'application/xml');
+  const title = dataParsed.querySelector('title').textContent;
+  const items = dataParsed.querySelectorAll('item');
+
+  return { title, items };
+};
+
 export default (() => {
-  console.log(`it works!`);
-
-  const state = {
-    form: {
-      status: 'filling',
-      valid: false,
-    },
-  };
-
-  const schema = yup.string().url().required();
-
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('input'),
+    feedback: document.querySelector('.feedback'),
+    sendButton: document.querySelector('button'),
+    feedsContainer: document.querySelector('.feeds'),
   };
-  console.log(elements.form);
 
-  elements.form.addEventListener('input', () => {
-    schema.validate(state)
-      .then(watch(elements, state).form.valid = true)
-      .catch((err) => {
-        console.log(err);
-        watch(elements, state).form.valid = false;
-      });
-  });
+  const state = {
+    sources: [],
+    feeds: [],
+    form: {
+      status: 'filling',
+      error: null,
+      valid: true,
+      value: '',
+    },
+    errors: {},
+  };
+
+  const { watchedState, unwatchedState } = watch(elements, state);
+
+  const schema = yup.string().url().required();
+
+  // elements.form.addEventListener('input', (event) => {
+  //   schema.validate(event.target.value)
+  //     .then(() => {
+  //       console.log('valid!');
+  //       watchedState.form.valid = true;
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       console.log('INvalid!');
+
+  //       watchedState.form.valid = false;
+  //     });
+  // });
+
   elements.form.addEventListener('submit', (event) => {
     event.preventDefault();
-    watch(elements, state).form.processState = 'sending';
+
+    const formData = new FormData(event.target);
+    const rssUrl = formData.get('rssUrl');
+
+    schema.validate(rssUrl)
+      .then(() => {
+        console.log('valid!');
+        watchedState.form.valid = true;
+
+        const corsUrl = 'https://cors-anywhere.herokuapp.com/';
+        const fullUrl = `${corsUrl}${rssUrl}`;
+
+        axios.get(fullUrl)
+          .then((response) => {
+            const { title, items } = parse(response.data);
+
+            const sourcesFiltered = unwatchedState.sources
+              .filter((source) => source.title === title);
+
+            if (sourcesFiltered.length !== 0) {
+              watchedState.form.status = 'doubleAdded';
+              return;
+            }
+
+            watchedState.form.status = 'sending';
+
+            const id = _.uniqueId();
+            watchedState.sources.push({ id, title });
+            watchedState.feeds.push({ id, title, items });
+            watchedState.form.status = 'submitted';
+          })
+          .catch((error) => {
+            console.log(error);
+            watchedState.form.status = 'failed';
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        watchedState.form.valid = false;
+      });
   });
 });
--- src/watchers.js
+++ src/watchers.js
@@ -1,18 +1,65 @@
 import onChange from 'on-change';
 
+const getPosts = (feeds) => {
+  const html = feeds.map((feed) => {
+    const title = `<h2>${feed.title}</h2>`;
+    const posts = [...feed.items].map((item) => {
+      const itemLink = item.querySelector('link').textContent;
+      const itemTitle = item.querySelector('title').textContent;
+      return `<div><a href="${itemLink}">${itemTitle}</a></div>`;
+    }).join('\n');
+    return [title, posts].join('\n');
+  }).join('\n');
+  return html;
+};
+
 export default (elements, state) => {
+  const {
+    input, feedback, sendButton, feedsContainer,
+  } = elements;
+
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
       case 'form.valid':
         if (value === false) {
-          console.log(`invalid`);
-          elements.input.classList.add('is-invalid');
+          input.classList.remove('is-valid');
+          input.classList.add('is-invalid');
+          feedback.textContent = 'Please enter a valid URL';
+          sendButton.disabled = true;
         }
+        if (value === true) {
+          input.classList.remove('is-invalid');
+          input.classList.add('is-valid');
+          feedback.textContent = '';
+          sendButton.disabled = false;
+        }
+        break;
+      case 'form.status':
+        if (value === 'doubleAdded') {
+          feedback.textContent = 'This RSS has already been added';
+        }
+        if (value === 'submitted') {
+          feedback.textContent = '';
+          input.value = '';
+          sendButton.disabled = false;
+        }
+        if (value === 'failed') {
+          feedback.textContent = 'Connection problems. Try again';
+        }
+        if (value === 'sending') {
+          feedback.textContent = 'Sending...';
+          sendButton.disabled = true;
+        }
+        break;
+      case 'feeds':
+        feedsContainer.innerHTML = getPosts(value);
         break;
       default:
         break;
     }
   });
 
-  return watchedState;
+  const unwatchedState = onChange.target(state);
+
+  return { watchedState, unwatchedState };
 };
